b1888b23b3153093f3b0114cae42919f0f1b5ed1
Saulo Lira <saulo.l.nascimento@hotmail.com>
2020-06-27 16:48:22 -0300

    Remova comentário não mais necessário

--- processamento-de-imagens/10-histograma.py
+++ processamento-de-imagens/10-histograma.py
@@ -85,7 +85,6 @@ plt.show()
 
 '''
 Outra forma de calcular e exibir os histogramas: Utilizando a função calcHist() e loop.
-Precisa importar a biblioteca numpy.
 (Método utilizado nas aulas de computação gráfica do IFAL)
 '''
 
